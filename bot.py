2873d05ce75574f2b9cf6907b4b2494ae61ec33a
DriftWare07 <99690970+DriftWare07@users.noreply.github.com>
2024-10-18 15:53:58 -0500

    restored changes

--- bot.py
+++ bot.py
@@ -11,6 +11,18 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
+        if message.author == client.user:
+            return
+        if message.content.startswith("test"):
+         channel = message.channel
+         await channel.send("output_message")
+
+#this function will send a message if the bot sees that another predefined message has been sent
+async def send_message_on(message, input_message, output_message):
+    
+    if message.content.startswith(input_message):
+         channel = message.channel
+         await channel.send(output_message)
 
 intents = discord.Intents.default()
 intents.message_content = True
